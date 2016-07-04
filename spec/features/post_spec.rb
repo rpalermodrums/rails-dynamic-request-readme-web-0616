add867fa1b8b7dc2bce081fc0ca46b92a1a8b32a
Ryan Palermo <stix250@gmail.com>
2016-07-04 16:46:51 -0400

    Done.

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -20,3 +20,16 @@ describe 'navigate' do
     expect(page).to have_css("p", text: "My post desc")
   end
 end
+
+require 'rails_helper'
+
+describe 'navigate' do
+  before do
+    @post = Post.create(title: "My Post", description: "My post desc")
+  end
+
+  it 'to post pages' do
+    visit "/posts/#{@post.id}"
+    expect(page.status_code).to eq(200)
+  end
+end
